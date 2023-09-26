8f46640d4d0f55a3d6d63564ffa1e10dc109a57b
Breno Macedo <brenonsmacedo@gmail.com>
2023-09-25 21:30:11 -0300

    desafio-feito

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ let heroi = {
 }
 
 if(heroi.xp < 1000){
-    console.log("Ferro");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Ferro ");
 }if (heroi.xp >= 1001 && heroi.xp <= 2000 ) {
-    console.log("Bronze");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Bronze");
 }if (heroi.xp >= 2001 && heroi.xp <= 5000 ){
-    console.log("Prata");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Prata");
 }if (heroi.xp >= 6001 && heroi.xp <= 7000 ){
-    console.log("Platina");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Platina");
 }if (heroi.xp >= 7001 && heroi.xp <= 8000 ){
-    console.log("Ascendente");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Ascendente");
 }if (heroi.xp >= 9000 && heroi.xp <= 10000 ){
-    console.log("Imortal");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Imortal");
 }if (heroi.xp >= 10001){
-    console.log("Radiante");
+    console.log("O herói de nome: " + heroi.nome + " está no nível de Radiante");
 }
 
-console.log("O herói de nome: " + heroi.nome + "está no nível de: " + heroi.xp);
